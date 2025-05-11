2914299904ac4370a85e08e385d62b436be750ba
Lumineskz <shubhamsulabh6@gmail.com>
2025-05-11 08:57:48 +0545

    Category filter works, need to add more items

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,26 @@
+/* For navbar menu */
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const mobileNav = document.querySelector('.mobile-nav');
 
 hamburgerMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('open');
 });
+/* For menu items image clicking in mobile */
 function toggleFontSize(menuImg) {
     let desc = menuImg.parentElement.querySelector('.desc');
     desc.style.fontSize = desc.style.fontSize === '0.8em' ? '0em' : '0.8em';
 }
+// For category filter
+document.querySelectorAll('.cat-row .cat').forEach(category => {
+    category.addEventListener('click', () => {
+        const selectedCategory = category.textContent.trim().toLowerCase().replace(/\s+/g, '-');
+        
+        document.querySelectorAll('.menu-item').forEach(item => {
+            if (item.getAttribute('data-category') === selectedCategory) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+});
