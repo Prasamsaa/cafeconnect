8138e1e375f046a9ac35f9f9b93cc834c16d5c04
Lumineskz <shubhamsulabh6@gmail.com>
2025-05-11 08:43:12 +0545

    Applied all script in one script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,3 +4,7 @@ const mobileNav = document.querySelector('.mobile-nav');
 hamburgerMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('open');
 });
+function toggleFontSize(menuImg) {
+    let desc = menuImg.parentElement.querySelector('.desc');
+    desc.style.fontSize = desc.style.fontSize === '0.8em' ? '0em' : '0.8em';
+}
