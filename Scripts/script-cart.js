c40dc436f0e6887efaa77c4e4afae72c9682d06c
Lumineskz <shubhamsulabh6@gmail.com>
2025-05-11 12:52:54 +0545

    Deleted 2 other scripts since we are only using one

--- Scripts/script-cart.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const mobileNav = document.querySelector('.mobile-nav');
-
-hamburgerMenu.addEventListener('click', () => {
-    mobileNav.classList.toggle('open');
-});
